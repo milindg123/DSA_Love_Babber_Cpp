697b003bf8f28de79deb86b66c3331d34ddf65ab
Milind Ghegadmal <97649757+milindg123@users.noreply.github.com>
2023-08-22 00:27:41 +0530

    First commit

---
+++ UserInput.cpp/input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // Input taken by user code
+
+    int n;
+    cout<< "Enter the value of n: ";
+    cin>>n;
+    cout<< "Value of n is : "<< n << endl;
+}
